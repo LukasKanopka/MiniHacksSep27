de277a0e324f249f7821cb2c4f78e03c7976ff85
yanka deshkovski <yankadeshkovski@ufl.edu>
2025-09-27 15:04:52 -0400

    cleardatabase

--- neo4j/src/main.py
+++ neo4j/src/main.py
@@ -1,6 +1,7 @@
 from . import db
 
 if __name__ == "__main__":
-    db.create_example_graph()
-    print("People who know someone:", db.get_people())
+    #db.create_example_graph()
+    db.clear_db()
+    #print("People who know someone:", db.get_people())
     db.close()
